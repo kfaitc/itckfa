9a6ae7b203923de20dfb2b1ce5cfcb841e6bc66c
kfaitc <137277425+kfaitc@users.noreply.github.com>
2023-11-07 13:21:41 +0700

    update Wallet feature

--- lib/screen/Home/Home.dart
+++ lib/screen/Home/Home.dart
@@ -11,6 +11,7 @@ import 'package:itckfa/Memory_local/database.dart';
 import 'package:itckfa/afa/components/contants.dart';
 import 'package:itckfa/afa/screens/Auth/login.dart';
 import 'package:itckfa/afa/screens/AutoVerbal/Add.dart';
+import 'package:itckfa/afa/screens/walletscreen.dart';
 import 'package:itckfa/screen/Abouts/Abouts.dart';
 import 'package:itckfa/screen/Abouts/aboutSideBar.dart';
 import 'package:itckfa/screen/Account/account.dart';
@@ -22,6 +23,7 @@ import 'package:itckfa/screen/Home/Body.dart';
 import 'package:itckfa/screen/Home/Customs/Drawer_menu.dart';
 import 'package:itckfa/screen/Home/Customs/MyDrawerList.dart';
 import 'package:itckfa/screen/Promotion/PartnerList.dart';
+import 'package:itckfa/screen/Property/Home_Screen_property.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -177,12 +179,25 @@ class _HomePageState extends State<HomePage1> {
             MyDrawerList(
               icon: Icons.villa,
               title: 'Property',
-              Press: () {},
+              Press: () {
+                 Navigator.push(context, MaterialPageRoute(
+                                      builder: (context) {
+                                        return Home_Screen_property();
+                                      },
+                                    ));
+              },
             ),
             MyDrawerList(
               icon: Icons.wallet,
               title: 'Wallet',
-              Press: () {},
+              Press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) {
+                    return Walletscreen();
+                  }),
+                );         
+              },
             ),
             MyDrawerList(
               icon: Icons.question_answer,
